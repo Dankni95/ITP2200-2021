12b340a49b38dd7f9d4381b9ce00f372636493b4
Dankni95 <Daniel_kn@live.no>
2021-03-28 14:37:57 +0200

    does the test run now?

--- src/test/java/ContactManagerTest.java
+++ src/test/java/ContactManagerTest.java
@@ -3,8 +3,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-
 class ContactManagerTest {
 
     ContactManager manager; // setup, initiate objects before each ... for setup
