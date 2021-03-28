0b7283d58c88a45ed09afa464540a7a982fac9c7
Dankni95 <Daniel_kn@live.no>
2021-03-28 21:07:50 +0200

    ready to pull?

--- src/test/java/ContactManagerTest.java
+++ src/test/java/ContactManagerTest.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+// successful pull and merge question mark
+
 class ContactManagerTest {
 
     ContactManager manager; // setup, initiate objects before each ... for setup
