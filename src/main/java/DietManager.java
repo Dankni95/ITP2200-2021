84cd26c4d3bdbc83eeceb7613133829d70dbb9cf
Daniel Lysak <Daniel_kn@live.no>
2021-04-09 11:41:33 +0200

    Skeleton draft

---
+++ src/main/java/DietManager.java
@@ -0,0 +1,9 @@
+public class DietManager {
+    public Error areCompatible(Person person, Diet diet) {
+        return new Error("Not implemented");
+    }
+
+    public Error randomDiet(Person person, Food[] foods) {
+        return new Error("Not implemented");
+    }
+}
---
+++ src/main/java/VeganDiet.java
@@ -0,0 +1,3 @@
+public class VeganDiet extends Diet{
+    private float minWeightKg;
+}
