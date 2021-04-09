b3d47e65001c302a0da7514c2bd40918feebfe65
harry <harry.mliam@gmail.com>
2021-04-09 11:59:45 +0200

    first commit

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,33 +1,4 @@
-<<<<<<< HEAD
-public class Person {
-    private String name;
-    private int age;
 
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-}
-
-
-=======
 import java.util.ArrayList;
 import java.util.List;
 
@@ -76,4 +47,3 @@ public class Person {
 
 
 }
->>>>>>> origin/master
